e2380421fd07db648916463dcceac7cbc3d74f29
Swen Mulderij <swenmulderij@gmail.com>
2022-11-04 18:13:32 +0100

    NEw selector

--- src/components/BioformSection/index.tsx
+++ src/components/BioformSection/index.tsx
@@ -4,37 +4,11 @@ import { bioforms } from "../../data/bioforms";
 import { Bioform } from "../../types/hero";
 import AdderBlocks from "../AdderBlocks";
 import { RoundedTop } from "../../styles/defaults";
+import Selector from "../Selector";
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-
-const PickerWrapper = styled(RoundedTop)`
-  width: 100%;
-  position: relative;
-  border-style: solid;
-
-  &::after {
-    content: "▼";
-    font-size: 2rem;
-    right: 2.4rem;
-    position: absolute;
-    margin: 1.2rem;
-    pointer-events: none;
-  }
-`;
-
-const Picker = styled(RoundedTop)`
-  padding: 1.2rem;
-  border: none;
-  -webkit-appearance: none;
-  appearance: none;
-  width: 100%;
-  background-color: ${(props) => props.theme.palette.common.background};
-  font-size: 2rem;
-  font-family: inherit;
-  cursor: pointer;
 `;
 
 const InfoText = styled.p`
@@ -73,7 +47,7 @@ const CurrentInfo = ({ selected }: { selected: Bioform | undefined }) => {
   return (
     <InfoWrapper>
       <InfoText>
-        <p>Select a race to see more info!</p>
+        <p>Select a bioform to see more info!</p>
       </InfoText>
     </InfoWrapper>
   );
@@ -83,20 +57,11 @@ const BioformSection = () => {
 
   return (
     <Wrapper>
-      <PickerWrapper>
-        <Picker
-          as="select"
-          value={hero.bioform?.name}
-          onChange={(e) =>
-            setBioform(bioforms.find((bf) => bf.name === e.target.value))
-          }
-        >
-          <option>Pick a race</option>
-          {bioforms.map((bf, i) => (
-            <option key={i}>{bf.name}</option>
-          ))}
-        </Picker>
-      </PickerWrapper>
+      <Selector
+        value={hero.bioform?.name}
+        onChange={(val) => setBioform(bioforms.find((bf) => bf.name === val))}
+        options={bioforms.map((bf) => bf.name)}
+      />
       <CurrentInfo selected={hero.bioform} />
     </Wrapper>
   );
---
+++ src/components/Selector/index.tsx
@@ -0,0 +1,77 @@
+import * as Select from "@radix-ui/react-select";
+import styled from "styled-components";
+
+const Item = styled(Select.Item)`
+  font-size: 2rem;
+  padding: 2rem;
+  border-top: dashed 1px;
+  &[data-highlighted] {
+    background-color: ${(props) => props.theme.palette.common.foreground};
+    color: ${(props) => props.theme.palette.common.background};
+    box-shadow: none;
+    border-radius: inherit;
+  }
+`;
+
+const Trigger = styled(Select.Trigger)`
+  background-color: ${(props) => props.theme.palette.common.background};
+  border-style: solid;
+  border-width: 1px;
+  border-radius: ${(props) => props.theme.borderRadiusTop};
+  color: ${(props) => props.theme.palette.common.text};
+  width: 100%;
+  padding: 1.5rem;
+  font-size: 2rem;
+  font-family: inherit;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+`;
+const Content = styled(Select.Content)`
+  background-color: ${(props) => props.theme.palette.common.background};
+  width: 80%;
+  border-style: solid;
+  border-width: 2px;
+  border-radius: ${(props) => props.theme.borderRadiusTop};
+
+  & :first-child {
+    border-top: none;
+  }
+`;
+
+const Selector = ({
+  options,
+  value,
+  onChange
+}: {
+  options: string[];
+  value: string | undefined;
+  onChange: (value: string) => void;
+}) => {
+  return (
+    <Select.Root onValueChange={onChange} value={value || ""}>
+      <Trigger>
+        <Select.Value placeholder="Select a bioform" />
+        <Select.Icon />
+      </Trigger>
+      <Select.Portal>
+        <Content>
+          <Select.ScrollUpButton />
+          <Select.Viewport>
+            <Item value={""}>
+              <Select.ItemText>Select a bioform</Select.ItemText>
+            </Item>
+            {options.map((child, i) => (
+              <Item value={child} key={i}>
+                <Select.ItemText>{child}</Select.ItemText>
+              </Item>
+            ))}
+          </Select.Viewport>
+          <Select.ScrollDownButton />
+        </Content>
+      </Select.Portal>
+    </Select.Root>
+  );
+};
+
+export default Selector;
